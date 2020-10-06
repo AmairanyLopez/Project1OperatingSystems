6969278b2eac17089ede37d17f86af3bafc3fc9b
AmairanyLopez <43075799+AmairanyLopez@users.noreply.github.com>
2020-10-05 23:19:57 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -206,7 +206,7 @@ int main(int argc, char **argv)
 				
 				// TO-DO P5.6
 				//push to stack to save commands 
-				push(exec_argv[0], stack, &top, stack_size);
+				push(*exec_argv[0], stack, &top, stack_size);
 				//fprintf(stderr, stack[top]);
 				return imthechild(exec_argv[0], &exec_argv[0]);
 				/* Exit from main. */
