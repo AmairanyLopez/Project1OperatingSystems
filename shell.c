cd800c96d4a4679a8a788e6db50d4b3816f76294
AmairanyLopez <43075799+AmairanyLopez@users.noreply.github.com>
2020-10-07 20:48:43 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -236,8 +236,7 @@ int main(int argc, char **argv)
 
 		}else if (!strncmp(exec_argv[0], "!", 1) && exec_argc > 1) {
 			//obtain command number
-			char punta[1] = exec_argv[0][1];
-			int cmndnum = atoi(punta)
+			char cmndnum = exec_argv[0][1];
 			(cmndnum>0 && cmndnum <= 9) ? fprintf(stderr, "Valid") : fprintf(stderr, "The number is invalid!");
 
 		} else {
