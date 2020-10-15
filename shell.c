55b347e0dd57491279382dcb65a819977ebbdf13
AmairanyLopez <43075799+AmairanyLopez@users.noreply.github.com>
2020-10-14 21:16:28 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -238,8 +238,8 @@ int main(int argc, char **argv)
 			//obtain command number
 			fprintf(stderr, "Enter the function to verify a resubmission of command");
 			char *xcmndnum = exec_argv[0];
-			char cmndnum=xcmndnum[1];
-			printf(cmndnum);
+			//char cmndnum=xcmndnum[1];
+			//printf(cmndnum);
 			
 			(cmndnum>0 && cmndnum <= 9) ? fprintf(stderr, "Valid") : fprintf(stderr, "The number is invalid!");
 
