647dcdd2f1d6d0ade928507714c4dbe8145ddd12
AmairanyLopez <43075799+AmairanyLopez@users.noreply.github.com>
2020-10-14 23:07:51 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -169,7 +169,7 @@ int main(int argc, char **argv)
 			
 			switch(c){
 				case '0':
-					printf(estack(c) ? estack(c) : "Invalid number");
+					printf((estack(c)[0]!='\0') ? estack(c) : "Invalid number");
 					break;
 				case '1':
 					printf("Second option great \n");
